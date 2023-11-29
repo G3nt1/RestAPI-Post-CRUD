3f2c714cbe9e7a707a6abcc5ab5bbf971257d6a7
G3nt1 <gentjanmateli77@gmail.com>
2023-11-29 14:37:46 +0100

    Delete, Details, Update a post

--- post/urls.py
+++ post/urls.py
@@ -5,4 +5,7 @@ from post import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("new-post", views.create_post, name="new-post"),
+    path("delete-post", views.delete_post, name="delete-post"),
+    path("get-post", views.get_post, name="get-post"),
+    path("update-post", views.update_post, name="update-post"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -23,3 +22,44 @@ def create_post(request):
         return Response({'success': 'The post created successfully'}, status=201)
     else:
         return Response(serialize.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def delete_post(request):
+    post_id = request.data.get('post_id')
+    try:
+        post = Post.objects.get(id=post_id)
+        post.delete()
+        return Response({'success': 'The post deleted successfully'}, status=200)
+    except Post.DoesNotExist:
+        return Response({'Error': 'The post does not exist'}, status=404)
+
+
+@api_view(['GET'])
+def get_post(request):
+    post_id = request.data.get('post_id')
+    try:
+        post = Post.objects.get(id=post_id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    except Post.DoesNotExist:
+        return Response({'Error': 'The post does not exist'})
+
+
+@api_view(['PUT'])
+def update_post(request):
+    post_id = request.data.get('post_id')
+    new_title = request.data.get('new_title')
+    new_content = request.data.get('new_content')
+
+    try:
+        post = Post.objects.get(id=post_id)
+        if new_title:
+            post.title = new_title
+        if new_content:
+            post.content = new_content
+        post.save()
+        return Response({'success': 'The post updated successfully Updated'}, status=200)
+
+    except Post.DoesNotExist:
+        return Response({'Error': 'The post does not exist'})
